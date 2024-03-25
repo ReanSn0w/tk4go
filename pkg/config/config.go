65d066de68b334edca0417aa7c9a7bef240f1331
Dmitriy Papkov <papkovda@me.com>
2024-03-25 18:36:29 +0300

    [Fix] loading configuration

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,13 +1,17 @@
 package config
 
 import (
+	"bytes"
+	"io"
 	"log"
+	"reflect"
 
+	"github.com/ReanSn0w/tk4go/pkg/tools"
 	"github.com/umputun/go-flags"
 )
 
 func Parse(opts any) error {
-	p := flags.NewParser(&opts, flags.PrintErrors|flags.PassDoubleDash|flags.HelpFlag|flags.IgnoreUnknown)
+	p := flags.NewParser(opts, flags.PrintErrors|flags.PassDoubleDash|flags.HelpFlag|flags.IgnoreUnknown)
 	p.SubcommandsOptional = true
 
 	if _, err := p.Parse(); err != nil {
@@ -18,4 +22,39 @@ func Parse(opts any) error {
 	}
 
 	return nil
+}
+
+func Print(log tools.Logger, title string, revision string, opts any) {
+	buf := new(bytes.Buffer)
+	structPrinter(buf, 0, opts)
+	log.Logf("\nApplication: %v (rev: %v) \n%s", title, revision, buf.String())
+}
+
+func structPrinter(b io.Writer, lvl int, v any) {
+	val := reflect.ValueOf(v)
+	if val.IsNil() {
+		return
+	}
+
+	switch val.Kind() {
+	case reflect.Ptr:
+		val = val.Elem()
+		structPrinter(b, lvl, val.Interface())
+		return
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			structPrinter(b, lvl+1, field.Interface())
+		}
+	default:
+		// print value
+		name := val.Type().Name()
+		val = reflect.Indirect(val)
+
+		for i := 0; i < lvl; i++ {
+			b.Write([]byte("  "))
+		}
+
+		b.Write([]byte(name + ": " + val.String() + "\n"))
+	}
 }
