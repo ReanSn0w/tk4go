09b4b0cb08ef0d76fd94b14112f923a4ac8271b8
Дмитрий Папков <reansnow@mac-mini--aceplace.local>
2024-05-03 10:52:47 +0300

    [Update] cycle.go
    - Теперь остановка задачи происходит через локальный контекст, создаваемый в момент запуска задачи по таймеру

--- pkg/tools/cycle.go
+++ pkg/tools/cycle.go
@@ -1,25 +1,24 @@
 package tools
 
 import (
+	"context"
 	"time"
 )
 
 // Функция создает новую структуру для решения
 // повторяющихся задач
 func NewCycleTask(task func()) *CycleTask {
-
 	return &CycleTask{
 		timer: time.NewTimer(0),
-		done:  make(chan bool),
 		task:  task,
 	}
 }
 
 // Структура для работы с циклической задачей
 type CycleTask struct {
-	timer *time.Timer
-	done  chan bool
-	task  func()
+	timer  *time.Timer
+	task   func()
+	cancel func()
 }
 
 // Запуск задачи единоразово здесь и сейчас
@@ -29,27 +28,28 @@ func (r *CycleTask) Once() {
 
 // Останавливает цикл выполнения задачи
 func (r *CycleTask) Stop() {
-	if r.timer.Stop() {
-		r.done <- true
+	if r.cancel != nil {
+		r.cancel()
 	}
 }
 
 // Запускает цикл выполнения задачи
 // в случае если задача уже запущена
 func (r *CycleTask) Run(d time.Duration) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	r.cancel = cancel
 	r.timer.Reset(d)
 
 	go func() {
-		loop := true
-
-		for loop {
+		for {
 			select {
 			case <-r.timer.C:
 				r.task()
 				r.timer.Reset(d)
-			case <-r.done:
-				r.timer.Stop()
-				loop = false
+			case <-ctx.Done():
+				r.cancel = nil
+				break
 			}
 		}
 	}()
